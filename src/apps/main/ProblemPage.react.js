dd194a0c6ec0c58c1418d0a637a51f0bf1b9c679
CMS <tyms0503@gmail.com>
2020-08-10 21:25:37 +0900

    add Nav

--- src/apps/main/ProblemPage.react.js
+++ src/apps/main/ProblemPage.react.js
@@ -2,15 +2,27 @@
 
 import * as React from "react";
 import axios from 'axios';
-import { Page, Grid, GalleryCard, Card, Button } from "tabler-react";
-
+import { Nav, Page } from "tabler-react";
+import { useSelector } from "react-redux"
 import SiteWrapper from "./SiteWrapper.react";
+import { Document, Page as pdf_page} from "react-pdf";
 
 function ProblemPage() {
     const [problem, setProblem] = React.useState([])
+    const selectedId = window.localStorage.getItem('selectedProblemId')
+    console.log("====>", selectedId)
     return(
         <SiteWrapper>
-
+            <Page.Content>
+                <Nav>
+                    <Nav.Item value={selectedId + "번"} />
+                    <Nav.Item to="http://www.example.com">제출</Nav.Item>
+                    <Nav.Item value="대전" />
+                    <Nav.Item value="리플레이"/>
+                    <Nav.Item value="내코드"/>
+                    <Nav.Item value="대전현황"/>
+                </Nav>
+            </Page.Content>
         </SiteWrapper>
     )
 }
