5032285a05dd937ebea0b13931cda296a93fe75a
CMS <tyms0503@gmail.com>
2020-08-12 23:39:51 +0900

    delete unused import

--- src/apps/App.react.js
+++ src/apps/App.react.js
@@ -1,35 +1,24 @@
 import * as React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import {
-//   ForgotPasswordPage,
-//   LoginPage,
-//   RegisterPage,
-//   Error400,
-//   Error401,
-//   Error403,
-//   Error404,
-//   Error500,
-//   Error503,
-//   Empty,
-//   Email,
-//   ProfilePage,
-// } from "../pages";
-
-import HomePage from "./main/HomePage.react";
-import ProblemListPage from "./main/ProblemListPage.react"
-import ProblemPage from "./main/ProblemPage.react"
+import Home from "./main/Home.react";
+import ProblemList from "./main/ProblemList.react"
+import Problem from "./main/problem/Problem.react"
+import * as problemNav from "./main/problem" 
 import "tabler-react/dist/Tabler.css";
-
-type Props = {||};
 
 function App(props: Props): React.Node {
   return (
     <React.StrictMode>
       <Router>
         <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/problems" component={ProblemListPage} />
-          <Route exact path="/problems/:id" component={ProblemPage} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/problem" component={ProblemList} />
+          <Route exact path="/problem/:id" component={Problem} />
+          <Route exact path="/submit/:id" component={problemNav.Submit} />
+          <Route exact path="/replay/:id" component={problemNav.Replay} />
+          <Route exact path="/match/:id" component={problemNav.match} />
+          <Route exact path="/code/my/:id" component={problemNav.code} />
+          <Route exact path="/matchlog/:id" component={problemNav.matchlog} />
         </Switch>
       </Router>
     </React.StrictMode>
