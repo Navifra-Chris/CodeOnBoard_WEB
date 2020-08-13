0b88dcca47d6d91dcfe1b2876ba4f7ebf7f48da9
CMS <tyms0503@gmail.com>
2020-08-13 23:48:24 +0900

    Add pdf and editor

--- src/apps/main/problem/components/ProblemViewer.react.js
+++ src/apps/main/problem/components/ProblemViewer.react.js
@@ -1,13 +1,42 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import * as React from "react";
+import { Button, Grid } from "tabler-react"
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
-function problemViewer()  {
-    console.log("===> viewer")
+function ProblemViewer(props)  {
+    {console.log("===> viewer", props)}
+    const [numPages, setNumPages] = React.useState(null);
+    const [pageNumber, setPageNumber] = React.useState(1);
+
+    const onDocumentLoadSuccess = ({ numPages }) => {
+        setNumPages(numPages);
+    };
+
     return(
-        <h1>viewer</h1>
+        <React.Fragment>
+        <Grid.Row justifyContent="center" className="pt-2">
+        <Document 
+        file={props.desc} 
+        onLoadSuccess={onDocumentLoadSuccess}>
+        <Page pageNumber={pageNumber} />
+        </Document>
+            <Grid.Row className="pt-2">
+                <Grid.Col >
+                    <Button color="priamry" disabled={pageNumber <=1} onClick={() => setPageNumber(prevPageNumber => prevPageNumber-1)}>이전</Button>
+                </Grid.Col>
+                <Grid.Col className="pt-2" auto={true}>
+                    <p>
+                        Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+                    </p>
+                </Grid.Col>
+                <Grid.Col>
+                    <Button color="priamry" disabled={pageNumber >=numPages} onClick={() => setPageNumber(prevPageNumber => prevPageNumber+1)}>다음</Button>
+                </Grid.Col>
+            </Grid.Row>
+        </Grid.Row>
+        </React.Fragment>   
     )
 }
 
-export default problemViewer;
+export default ProblemViewer;
