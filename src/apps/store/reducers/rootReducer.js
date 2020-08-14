d5c619763cf48360844adf771c5cb6d935b96a8e
CMS0503 <tyms0503@gmail.com>
2020-08-15 01:11:19 +0900

    Add match reducer

--- src/apps/store/reducers/rootReducer.js
+++ src/apps/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import problem from './problem.reducer';
+import match from './match.reducer'
 
 const rootReducer = combineReducers({
   problem,
+  match
 });
 
 export default rootReducer;
