cd6b27e158bf934023f1728d04b7da84cafa9579
CMS0503 <tyms0503@gmail.com>
2020-08-14 19:52:03 +0900

    Init

--- src/apps/main/problem/match.react.js
+++ src/apps/main/problem/match.react.js
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import axios from 'axios';
-import { Nav, Page } from "tabler-react";
+import { Card, Page, Grid, Avatar, Text } from "tabler-react";
 import { useSelector } from "react-redux"
 import SiteWrapper from "apps/main/SiteWrapper.react";
 import ProblemNav from "apps/main/problem/problemNav.react"
@@ -12,7 +12,24 @@ export const match = ({match}) => {
         <SiteWrapper>
             <Page.Content>
                 <ProblemNav id={match.params.id} />
-                    <h1> match </h1>
+                    <Grid.Row>
+                        <Grid.Col>
+                            <Card xl={4} className="mt-8 modal-dialog-centered" title="user 1">
+                                <Avatar className= "mt-2" icon="users" size="xxl"/>
+                                <Text className="mt-1" size="h2">user1</Text>
+                            </Card>
+                            
+                        </Grid.Col>
+                        <Grid.Col>
+                            <Card xl={4} body="대전"></Card>
+                        </Grid.Col>
+                        <Grid.Col>
+                            <Card xl={4} className="mt-8 modal-dialog-centered" title="user 2">
+                                <Avatar className= "mt-2" icon="users" size="xxl"/>
+                                <Text className="mt-1" size="h2">user2</Text>
+                            </Card>
+                        </Grid.Col>
+                    </Grid.Row>
             </Page.Content>
         </SiteWrapper>
     )
