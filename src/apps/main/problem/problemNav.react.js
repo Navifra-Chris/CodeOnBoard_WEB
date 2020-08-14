f37eeba0e46416d1bbf4b11199efdb8c25c0888e
CMS0503 <tyms0503@gmail.com>
2020-08-15 01:09:40 +0900

    Fix concat link

--- src/apps/main/problem/problemNav.react.js
+++ src/apps/main/problem/problemNav.react.js
@@ -8,11 +8,11 @@ function ProblemNav(props) {
             <Grid.Row justifyContent="center">
                 <Grid.Col auto={true}>
                     <Nav>
-                        <Nav.Item value={`${props.id}` + "번"} to={"/problem/" + `${props.id}`} />
-                        <Nav.Item to={"/match/" + `${props.id}`}>대전</Nav.Item>
-                        <Nav.Item to={"/replay/" + `${props.id}`}>리플레이</Nav.Item>
-                        <Nav.Item to={"/code/my/" + `${props.id}`}>내코드</Nav.Item>  
-                        <Nav.Item to={"/matchlog/" + `${props.id}`}>대전현황</Nav.Item>
+                        <Nav.Item value={`${props.id}번`} to={`/problem/${props.id}`} />
+                        <Nav.Item to={`/match/${props.id}`}>대전</Nav.Item>
+                        <Nav.Item to={`/replay/${props.id}`}>리플레이</Nav.Item>
+                        <Nav.Item to={`/code/my/${props.id}`}>내코드</Nav.Item>  
+                        <Nav.Item to={`/matchlog/${props.id}`}>대전현황</Nav.Item>
                     </Nav>
                 </Grid.Col>
             </Grid.Row>
