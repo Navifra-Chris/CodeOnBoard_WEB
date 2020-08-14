e173a6b8dff382a77439f55c7b932fb4f9023dd3
CMS0503 <tyms0503@gmail.com>
2020-08-14 19:50:48 +0900

    add react-redux and alert

--- src/apps/main/problem/Problem.react.js
+++ src/apps/main/problem/Problem.react.js
@@ -2,47 +2,49 @@
 
 import * as React from "react";
 import axios from 'axios';
-import { Card, Page, Grid } from "tabler-react";
-import { useSelector } from "react-redux"
+import { Alert, Page, Grid } from "tabler-react";
 import SiteWrapper from "../SiteWrapper.react";
-import { Document, Page as pdf_page} from "react-pdf";
 import ProblemNav from "apps/main/problem/problemNav.react"
 import ProblemViewer from "apps/main/problem/components/ProblemViewer.react"
 import CodeEditor from "apps/main/problem/components/CodeEditor.react"
+import { useSelector, useDispatch } from "react-redux"
+import * as Action from "apps/store/actions/problem.action";
 
 function Problem( {match} ) {
     // const selectedId = window.localStorage.getItem('userName'); Change "/codes/my" to "/codes/userName"
     const selectedProblemId = window.localStorage.getItem("selectedProblemId")
-    const selectedNavId = useSelector(state => state.problem.selected_nav_id, []);
-    console.log("===> selected nav id", selectedNavId);
+    const dispatch = useDispatch();
+    const problemIsSubmit = useSelector(state => state.problem.isSubmit, []);
+    const problemDesc = useSelector(state => state.problem.desc, []);
     
-    const [problem, setProblem] = React.useState([]);
+    let _alert;
+    if(problemIsSubmit === true){
+        console.log("ALERT")
+        _alert = <Alert type="success" icon="check">제출 완료</Alert>
+    }
 
     React.useEffect(() => {
         axios
         .get(`https://cors-anywhere.herokuapp.com/http://203.246.112.32:8000/api/v1/problem/${selectedProblemId}`,{})
-        .then(response =>
-            // {_desc = response.data.description
-            {setProblem(response.data);
+        .then(response =>{
+            dispatch(Action.getDescription(response.data.description))
+            console.log("===> problem", response.data)
         })
     },[]);
     
     return(
         <SiteWrapper>
             <Page.Content>
-                {console.log("problem ==>",problem)}
-                <ProblemNav id={match.params.id}/>
+            <ProblemNav id={match.params.id}/>
+            {_alert}
                 <Grid.Row>
                     <Grid.Col sm={6} lg={6}>
-                        <ProblemViewer desc={problem.description} />
+                        <ProblemViewer desc={problemDesc} />
                     </Grid.Col>
                     <Grid.Col sm={6} lg={6}>
                         <CodeEditor mode="post"></CodeEditor>                        
                     </Grid.Col>
                 </Grid.Row>
-
-
-
             </Page.Content>
             
         </SiteWrapper>
