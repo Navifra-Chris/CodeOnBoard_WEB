7c15341a54160c3dc9c53da73cbce28dfb41dbea
CMS0503 <tyms0503@gmail.com>
2020-08-14 19:55:17 +0900

    change reducer list

--- src/apps/store/reducers/rootReducer.js
+++ src/apps/store/reducers/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import problem from './problem.reducer';
-import getProblems from './getProblems.reducer';
 
 const rootReducer = combineReducers({
   problem,
-  getProblems
 });
 
 export default rootReducer;
