5302c4286e37649c5970e9f939ea05d6e8a747ef
CMS0503 <tyms0503@gmail.com>
2020-08-14 19:53:42 +0900

    Add functions

--- src/apps/store/actions/problem.action.js
+++ src/apps/store/actions/problem.action.js
@@ -1,10 +1,45 @@
-export const CHANGE_NAV = "CHANGE_NAV"
+export const SUBMIT = "SUBMIT"
+export const GET_DESCRIPTION = "GET_DESCRIPTION"
+export const WRITE_CODE = "WRITE_CODE"
+export const WIRTE_CODENAME = "WIRTE_CODENAME"
+export const SET_LANGUAGE = "SET_LANGUAGE"
 
-export function changeNav(param){
-    console.log("===> Change Nac ")
+export function submit(param){
+    console.log("===> SUBMIT")
     return {
-        type: CHANGE_NAV,
-        id: param
+        type: SUBMIT,
+        payload: param
     };
 }
+
+export function getDescription(param){
+    console.log("===> GET_DESCRIPTION")
+    return {
+        type: GET_DESCRIPTION,
+        payload: param
+    };
+}
+
+export function writeCode(param){
+    console.log("===> WRITE_CODE", param)
+    return {
+        type: WRITE_CODE,
+        payload: param
+    };
+}
+export function writeCodeName(param){
+    console.log("===> WIRTE_CODENAME",param)
+    return {
+        type: WIRTE_CODENAME,
+        payload: param
+    };
+}
+export function setLanguage(param){
+    console.log("===> SET_LANGUAGE")
+    return {
+        type: SET_LANGUAGE,
+        payload: param
+    };
+}
+
 
