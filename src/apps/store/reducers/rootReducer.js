994f68ebcfc898fe58806d7724450d0cb8a1b764
CMS <tyms0503@gmail.com>
2020-08-16 18:45:06 +0900

    Add codeList reducer

--- src/apps/store/reducers/rootReducer.js
+++ src/apps/store/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import problem from './problem.reducer';
 import match from './match.reducer'
+import codeList from './codeList.reducer'
 
 const rootReducer = combineReducers({
   problem,
-  match
+  match,
+  codeList
 });
 
 export default rootReducer;
