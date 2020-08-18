eed379329cb28e03f137f525782a7152c5af63f1
CMS <tyms0503@gmail.com>
2020-08-19 01:58:26 +0900

    Add replay

--- src/apps/store/reducers/rootReducer.js
+++ src/apps/store/reducers/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import problem from './problem.reducer';
 import match from './match.reducer'
 import codeList from './codeList.reducer'
+import replay from './replay.reducer'
 
 const rootReducer = combineReducers({
   problem,
   match,
-  codeList
+  codeList,
+  replay
 });
 
 export default rootReducer;
