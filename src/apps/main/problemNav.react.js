c8176b149fc0470afb29c392ed0e4b4cfc6db176
CMS0503 <tyms0503@gmail.com>
2020-08-20 01:54:54 +0900

    Add withRouter(NavLink)

--- src/apps/main/problemNav.react.js
+++ src/apps/main/problemNav.react.js
@@ -1,5 +1,6 @@
 import { Nav, Grid } from "tabler-react";
 import * as React from "react";
+import { NavLink, withRouter } from "react-router-dom";
 
 function ProblemNav(props) {
     // const selectedId = window.localStorage.getItem('userName'); Change "/codes/my" to "/codes/userName"
@@ -8,11 +9,11 @@ function ProblemNav(props) {
             <Grid.Row justifyContent="center">
                 <Grid.Col auto={true}>
                     <Nav>
-                        <Nav.Item value={`${props.id}번`} to={`/problem/${props.id}`} />
-                        <Nav.Item to={`/match/${props.id}`}>대전</Nav.Item>
-                        <Nav.Item to={`/replay/${props.id}`}>리플레이</Nav.Item>
-                        <Nav.Item to={`/code/my/`}>내코드</Nav.Item>  
-                        <Nav.Item to={`/matchlog/${props.id}`}>대전현황</Nav.Item>
+                        <Nav.Item LinkComponent={withRouter(NavLink)} value={`${props.id}번`} to={`/problem/${props.id}`} />
+                        <Nav.Item LinkComponent={withRouter(NavLink)} to={`/match/${props.id}`}>대전</Nav.Item>
+                        <Nav.Item LinkComponent={withRouter(NavLink)} to={`/replay/${props.id}`}>리플레이</Nav.Item>
+                        <Nav.Item LinkComponent={withRouter(NavLink)} to={`/code/my/`}>내코드</Nav.Item>  
+                        <Nav.Item LinkComponent={withRouter(NavLink)} to={`/matchlog/${props.id}`}>대전현황</Nav.Item>
                     </Nav>
                 </Grid.Col>
             </Grid.Row>
