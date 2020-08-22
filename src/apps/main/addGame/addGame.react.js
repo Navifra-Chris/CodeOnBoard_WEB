f94522341bdcb3fbe048b37dbec834420e500043
tyms0503 <tyms0503@gmail.com>
2020-08-22 22:04:18 +0900

    Add addGame

---
+++ src/apps/main/addGame/addGame.react.js
@@ -0,0 +1,21 @@
+// @flow
+
+import * as React from "react";
+import axios from 'axios';
+import { Button, Page, Card, Table, Loader } from "tabler-react";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
+import SiteWrapper from "../SiteWrapper.react";
+
+function AddGame({match}) {
+    
+
+    return(
+        <SiteWrapper>
+            <Page.Content>
+                
+            </Page.Content>
+        </SiteWrapper>
+    )
+};
+
+export default AddGame;
